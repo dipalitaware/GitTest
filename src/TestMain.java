714e4986c69243618b54089e8d89a39038f70c4d
Dipali Taware <dipalitaware005@gmail.com>
2023-08-04 22:21:01 +0530

    second commit done

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,7 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		System.out.println("This is a git hub test project.");
-
+		System.out.println("Practice makes man perfect!");
 	}
 
 }
